4f6aee673c47fc10fcb0bf227261d02222ba82e7
wangyesheng <wangyesheng@uniontech.com>
2025-05-27 12:18:34 +0800

    add finddevice
    Signed-off-by: wangyesheng <wangyesheng@uniontech.com>

--- main.cpp
+++ main.cpp
@@ -47,18 +47,116 @@ int main(int argc, char *argv[])
 
     // createWiredConnection("test4", "enp3s0f4u1u1");
 
+    QString findDeviceByName(const QString & iface);
+
     void activateWiredConnection(const QString &connectionName, const QString &interfaceName);
 
-    // Example: Activate a connection
-    activateWiredConnection("test4", "enp3s0f4u1u1");
+    void deactivateWiredConnection(const QString &connectionName);
+
+    // QString m_devicePath = "/org/freedesktop/NetworkManager/Devices/3";
+
+    // test
+    // QString m_devicePath = findDeviceByName("enp7s18u5u3c2");
+    // NetworkManager::Device::Ptr targetDevice = NetworkManager::findNetworkInterface(m_devicePath);
+    // NetworkManager::Device::Ptr targetDevice = findDeviceByName(interfaceName);
+    // The name of the device's data interface when available, becoming ACTIVATED.
+    // NetworkManager::Device::Ptr m_device = NetworkManager::findDeviceByIpFace(targetDevice->ipInterfaceName());
+    // qInfo() << "Target Device:" << targetDevice->interfaceName();
+    // qInfo() << "Target Device:" << m_device->interfaceName();
+
+    // test
+    // activateWiredConnection("test3", "enp7s18u5u3c2");
+
+    // test
+    deactivateWiredConnection("test3");
 
     // Example: Create and activate new connection
     return app.exec();
 }
 
+void listDevices()
+{
+    QList<NetworkManager::Device::Ptr> devices = NetworkManager::networkInterfaces();
+
+    if (devices.isEmpty()) {
+        qDebug() << "No network devices found.";
+    }
+
+    foreach (const NetworkManager::Device::Ptr &device, devices) {
+        QString devType;
+
+        switch (device->type()) {
+        case NetworkManager::Device::Ethernet:
+            devType = "Ethernet";
+            break;
+        case NetworkManager::Device::Wifi:
+            devType = "Wi-Fi";
+            break;
+        case NetworkManager::Device::Generic:
+            devType = "Generic";
+            break;
+        default:
+            devType = "Unknown";
+            break;
+        }
+
+        qDebug() << "----------------------------------------";
+        qDebug() << "Name:" << device->interfaceName();
+        qDebug() << "Type:" << devType;
+        qDebug() << "State:" << device->state();
+        qDebug() << "Uni (D-Bus Path):" << device->uni();
+
+
+        if (device->type() == NetworkManager::Device::Ethernet) {
+            NetworkManager::WiredDevice::Ptr wiredDevice =
+                qSharedPointerDynamicCast<NetworkManager::WiredDevice>(device);
+            if (wiredDevice) {
+                qDebug() << "MAC Address:" << wiredDevice->permanentHardwareAddress();
+            }
+        }
+    }
+}
+
+QString findDeviceByName(const QString & iface)
+{
+    QList<NetworkManager::Device::Ptr> devices = NetworkManager::networkInterfaces();
+
+    foreach (const NetworkManager::Device::Ptr &device, devices) {
+        if (device->interfaceName() == iface) {
+            qInfo() << "D-Bus object path." << device->uni();
+            return device->uni(); // This is the D-Bus object path
+        }
+    }
+
+    return QString();
+}
+
 void activateWiredConnection(const QString &connectionName, const QString &interfaceName)
 {
     NetworkManager::Connection::List connections = NetworkManager::listConnections();
+
+    // NetworkManager::WiredDevice::List devices = NetworkManager::WiredDevice::List();
+
+    // for (const NetworkManager::WiredDevice::Ptr &device : devices){
+    //     if(device->interfaceName() == interfaceName){
+    //         NetworkManager::Device::Ptr targetDevice = device;
+    //         qInfo() << "Target Device:" << targetDevice->interfaceName();
+    //     }
+
+    // }
+
+    // TODO, how to list all devices, and Path to name
+
+    findDeviceByName(interfaceName);
+    // QString m_devicePath = "/org/freedesktop/NetworkManager/Devices/3";
+    QString m_devicePath = findDeviceByName(interfaceName);
+    NetworkManager::Device::Ptr targetDevice = NetworkManager::findNetworkInterface(m_devicePath);
+    // NetworkManager::Device::Ptr targetDevice = findDeviceByName(interfaceName);
+    // The name of the device's data interface when available, becoming ACTIVATED.
+    // NetworkManager::Device::Ptr m_device = NetworkManager::findDeviceByIpFace(targetDevice->ipInterfaceName());
+    qInfo() << "Target Device:" << targetDevice->interfaceName();
+    // qInfo() << "Target Device:" << m_device->interfaceName();
+
 
     for (const NetworkManager::Connection::Ptr &connection : connections) {
         if (connection->name() == connectionName) {
@@ -66,9 +164,10 @@ void activateWiredConnection(const QString &connectionName, const QString &inter
             // connection = connectionName;
             // device = interfaceName;
             // specificObject = connection->path();
+
             // NetworkManager::activateConnection(connection, device, specificObject); // "/" = default device
             qInfo() << "Activating connection:" << connectionName << "interface: " << interfaceName << "Path: " << connection->path();
-            NetworkManager::activateConnection(connectionName, interfaceName, connection->path());
+            NetworkManager::activateConnection(connection->path(), targetDevice->uni(), "");
             qDebug() << "Activated connection:" << connectionName;
             return;
         }
@@ -82,7 +181,7 @@ void deactivateWiredConnection(const QString &connectionName)
     for (const NetworkManager::ActiveConnection::Ptr &active : NetworkManager::activeConnections()) {
         if (active->connection()->name() == connectionName) {
             NetworkManager::deactivateConnection(active->path());
-            qDebug() << "Deactivated connection:" << connectionName;
+            qInfo() << "Deactivated connection:" << connectionName;
             return;
         }
     }
